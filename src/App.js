e62913adca7fb8d08112b27fe45b44ed60b9bcdd
Biswajit <ascebiswajit@gmail.com>
2024-10-21 02:31:33 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -9,14 +9,8 @@ function App() {
         <p>
           THis is the  <code>CI-CD</code> Demo Projects
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+
           Learn React
-        </a>
       </header>
     </div>
   );
